f41babf3da84480a56693f97e60e44f6ddc30d8b
Kevin Blair <kblair40@gmail.com>
2023-11-25 05:51:37 -0600

    Finish express setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ const express = require("express");
 
 const app = express();
 
+// Do as much cpu work as possible, for some set 'duration'
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
 app.get("/", (req, res) => {
+  // doWork(5000) will delay the response by 5 seconds (5000ms);
+  doWork(5000);
   res.send("Worked");
 });
 
