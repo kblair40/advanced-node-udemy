77e514a344cb91f7adffdc00d982bc6342ec38e2
Kevin Blair <kblair40@gmail.com>
2023-11-26 08:25:24 -0600

    Add key creation function

--- services/cache.js
+++ services/cache.js
@@ -7,5 +7,15 @@ mongoose.Query.prototype.exec = function () {
   // All code here, before 'return exec.apply(...)' will run before a query is executed
   console.log("\nAbout to run a query");
 
+  // 'this' is the current query
+  console.log("Query:", this.getQuery());
+  console.log("collection", this.mongooseCollection.name);
+
+  // copy getQuery func so it's return value is not accidentally modified
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+  console.log("key:", key);
+
   return exec.apply(this, arguments);
 };
